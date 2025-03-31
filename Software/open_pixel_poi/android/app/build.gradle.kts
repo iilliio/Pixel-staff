8a9f53dbc84922e9186afcd397215f3b84e362f1
Mitch Lustig <Mitchlol@users.noreply.github.com>
2025-03-31 15:10:59 -0700

    Rename package

--- Software/open_pixel_poi/android/app/build.gradle.kts
+++ Software/open_pixel_poi/android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.open_pixel_poi"
+        applicationId = "com.mitchelllustig.open_pixel_poi"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
